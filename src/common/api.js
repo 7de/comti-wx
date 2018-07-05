a0140e21c8c042a9f8623b460fc549ea11a7f4ab
7de <252691878@qq.com>
2018-07-05 15:32:16 +0800

    最新代码更新

--- src/common/api.js
+++ src/common/api.js
@@ -7,67 +7,143 @@ import wepy from 'wepy'
 
 export default {
   apiData : {
-    host: ''
+    host: 'https://www.comtti.net/',
+    api: 'https://www.comtti.net/weChat/',
+    receiving: false, // 是否正在获取token
+    token: '',
+    userInfo: null,
+    code: null
   },
-    /* getRandomReply (id) {
-        let template = m_reply[id];
-        if (!template)
-            template = m_reply['other'];
+  _login() {
+    let {
+      code,
+      userInfo
+    } = this.$parent
+    return new Promise((resolve, reject) => {
+      if (code && userInfo){
+        resolve({
+          code,
+          userInfo
+        })
+      } else {
+        wepy.login({
+          success: res => {
+            this.$parent.code = res.code
+            wepy.showModal({
+              title: '提示',
+              content: '我们需要获得您的授权，才能进行下一步操作，请点击去授权',
+              showCancel: false,
+              confirmText: '去授权',
+              success: function(res) {
+                if (res.confirm) {
+                  wepy.redirectTo({
+                    url: './authorize'
+                  })
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+  },
+  _getToken() {
+    const {
+      api,
+      token,
+      receiving
+    } = this.apiData
+    return new Promise((resolve,reject) => {
+      if (receiving) { // 请求中
+        console.log('token请求中,等待1s后重新发送新的请求')
+        setTimeout(() => {
+          resolve(this._getToken())
+        }, 1000)
+      } else if (token) { // 如果缓存有token，直接获取缓存中的token
+        resolve(token)
+      } else {
+        this.apiData.receiving = true
+        this._login()
+      }
+    })
 
-        let index = Math.random() * template.length >> 0;
-
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(template[index]);
-            });
-        });
-
-    },
-    getContact () {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(m_contacts);
-            });
-        });
-    }, */
-
+  },
     getUserInfo () {
-      return new Promise((resolve, reject) => {
-        let cache = global.getUserInfo()
-        if (cache) {
-            resolve(cache)
-        } else { 
-          wepy.login().then((res) => {
-            console.log(res)
-            /* wepy.getUserInfo().then((res) => {
-              console.log('getuserinfo success')
-              console.log(res)
-              // global.setUserInfo(res.userInfo)
-              resolve(res.userInfo)
-            }).catch(reject) */
-          }).catch(reject)
+    },
+
+    // 获取用户标识
+    getCode () {
+      let _code = null
+      wepy.getStorage({
+        key: '_code',
+        success: res => {
+          _code = res.data
+          console.log(_code)
         }
       })
+      return _code
     },
+
+    getStorage(key) {
+      let _keydata = null
+      wepy.getStorage({
+        key: key,
+        success: res => {
+          _keydata = res.data
+          // this.getKeyData(_keydata)
+        }
+      })
+      console.log(_keydata)
+    },
+    getKeyData(data) {
+      console.log(data)
+      return data
+    },
+    /* // 获取storage
+    getStorage(key,_data) {
+      wepy.getStorage({
+        key: key,
+        success: function(res) {
+          _data = res.data
+          console.log(_data)
+        } 
+      })
+    } */
 
     // left join contact c
     // on (h.from == c.id or h.to == c.id)
     // where h.from = :id or h.to = :id or :id = '';
     // order by h.time asc;
-    getHistory (id) {
+    /* getHistory (id) {
       let history = wepy.getStorageSync('_wechat_history_') || m_history;
       return new Promise((resolve, reject) => {
         setTimeout(() => {
           let sorted = history.sort((a, b) => a.time - b.time)
-            /* if (!id)
+            if (!id)
                 resolve(this.leftJoin(sorted, m_contacts))
             else {
                 resolve(this.leftJoin(sorted.filter((v) => v.from === id || v.to === id), m_contacts));
-            } */
+            }
+        })
+      })
+    }, */
+    _token(){
+        // let _keydata = null
+      return new Promise((resolve, reject) => {
+        wepy.getStorage({
+          key: 'token',
+          success: res => {
+            resolve(res.data)
+          }
         })
       })
     },
     _request(method, url, params, header = {}) {
+      /* this._token().then( res => {
+        // console.log(res)
+        this.token = res
+        console.log(this.token)
+      }) */
       const {
         host
       } = this.apiData
@@ -83,36 +159,36 @@ export default {
             const {
               data
             } = res
-            if (data.code === 401) {
-              resolve(this._request(method, url, params))
+            if (data.code === 500 || data.status === 500) {
+              wepy.showToast({
+                title: '服务器错误，请联系管理员',
+                icon: 'none',
+                duration: 2000
+              })
+              // resolve(this._request(method, url, params))
+            } else if (data.code === -1) {
+              wepy.showToast({
+                title: data.msg ? data.msg : data.message,
+                icon: 'none',
+                duration: 2000
+              })
+            } else if (data.code === -100) {
+              /* wepy.showToast({
+                title: '授权过期',
+                icon: 'none',
+                duration: 2000
+              }) */
+              wepy.redirectTo({
+                url: '/pages/authorize'
+              })
             } else {
               resolve(data)
             }
+          },
+          fail: err => {
+            console.log('请求失败：' + err)
           }
         })
-        /* this._getToken().then(token => {
-          wepy.request({
-            url: `${api}${url}?access-token=${token}`,
-            method: method,
-            data: params,
-            header: Object.assign({
-              'content-type': 'application/json'
-            }, header),
-            success: res => {
-              const {
-                data
-              } = res
-              if (data.code === 401) {
-                this.globalData.code = ''
-                this.globalData.userinfo = ''
-                this.globalData.token = ''
-                resolve(this._request(method, url, params))
-              } else {
-                resolve(data)
-              }
-            }
-          })
-        }) */
       })
     },
     get(url, params = {}, header = {}) {
@@ -127,92 +203,77 @@ export default {
     delete(url, params = {}, header = {}) {
       return this._request('DELETE', url, params, header)
     },
-    // select *, (select msg from history h2 where h2.from = c.id or h2.to = c.id order by time desc limit 1) as lastmsg
-    // from history h
-    // left join contact c
-    // on (h.from == c.id or h.to == c.id)
-    // where h.from = :id or h.to = :id or :id = '';
-    // order by h.time desc;
-    /* getMessageList () {
-        let history = wepy.getStorageSync('_wechat_history_') || m_history;
-        return new Promise((resolve, reject) => {
-            let distince = [];
-            let rst = [];
-            let sorted = history.sort((a, b) => b.time - a.time);
-            sorted.forEach((v) => {
-                if (v.from !== 'me' && distince.indexOf(v.from) === -1) {
-                    distince.push(v.from);
-                }
-                if (v.to !== 'me' && distince.indexOf(v.to) === -1) {
-                    distince.push(v.to);
-                }
-            });
-
-            distince.forEach((v) => {
-                let pmsg = sorted.filter((msg) => msg.to === v || msg.from === v);
-                let lastmsg = pmsg.length ? pmsg[0].msg : '';
-
-                rst.push({
-                    id: v,
-                    lastmsg: lastmsg,
-                });
-            });
-
-            setTimeout(() => {
-                resolve(this.leftJoin(rst, m_contacts));
-            });
-        });
-    }, */
-
-    /* leftJoin(original, contacts) {
-
-        let contactObj = global.getContact();
-        let rst = [];
-
-        original.forEach((v) => {
-            if (!v.id) {
-                v.id = (v.from !== 'me') ? v.from : v.to;
-            }
-            if (v.id) {
-                if (v.id !== 'me') {
-                    v.name = contactObj[v.id].name;
-                    v.icon = (wepy.env === 'web' ? './mocks/users/' : '../mocks/users/') + contactObj[v.id].id + '.png';
-                }
-                rst.push(v);
-            }
-        });
-        return rst;
-    }, */
-
-    /* sendMsg (to, msg, type = 'text') {
-        return this.msg('me', to, msg, type);
-    }, */
-
-    /* replyMsg (frm, msg, type = 'text') {
-        return this.msg(frm, 'me', msg, type);
-    }, */
-
-    /* msg (frm, to, msg, type = 'text') {
-        let history = wepy.getStorageSync('_wechat_history_') || m_history;
-        let msgObj = {
-            to: to,
-            msg: msg,
-            type: type,
-            from: frm,
-            time: +new Date()
-        };
-
-        history.push(msgObj);
-
-        return new Promise((resolve, reject) => {
-            wepy.setStorage({key: '_wechat_history_', data: history}).then(() => {
-                resolve(msgObj);
-            }).catch(reject);
-        });
-    }, */
-
     clearMsg (id) {
       return wepy.clearStorage()
+    },
+    // 毫秒转换成时分秒
+    formatDuring (mss) {
+      // var days = parseInt(mss / (1000 * 60 * 60 * 24));
+      var hours = parseInt((mss % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = parseInt((mss % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = parseInt((mss % (1000 * 60)) / 1000);
+      // return days + " : " + hours + " : " + minutes + " : " + seconds;
+      return  (hours < 10 ? '0' + hours : hours) + " : " + (minutes < 10 ? '0' + minutes : minutes) + " : " + (seconds < 10 ? '0' + seconds : seconds);
+    },
+    // 毫秒转分钟
+    formatMin(time) {
+      return parseInt((time % (1000 * 60 * 60)) / (1000));
+    },
+    // 时间戳转换时间
+    formatDate (date, fmt) {
+      if (/(y+)/.test(fmt)) {
+          fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+      }
+      let o = {
+          'M+': date.getMonth() + 1,
+          'd+': date.getDate(),
+          'h+': date.getHours(),
+          'm+': date.getMinutes(),
+          's+': date.getSeconds()
+      };
+      for (let k in o) {
+          if (new RegExp(`(${k})`).test(fmt)) {
+              let str = o[k] + '';
+              fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : this.padLeftZero(str));
+          }
+      }
+      return fmt;
+    },
+
+    // 不足10，前加零
+    padLeftZero (str) {
+      return ('00' + str).substr(str.length);
+    },
+
+    // 小时转换毫秒
+    // 1时(h)=3600000毫秒(ms)
+    // 1分(min)=60000毫秒(ms)
+    // 1秒(s)=1000毫秒(ms)
+    formatMs(time) {
+      return time * 3600000
+    },
+    // 时分秒计时器
+    startTime(h,m,s) {
+      let _h = h
+      let _m = m
+      let _s = s
+      _h = this.checkTime(_h)
+      _m = this.checkTime(_m)
+      _s = this.checkTime(_s)
+    },
+    checkTime(i) {
+      if (i<10) {i="0" + i}
+      return i
+    },
+
+    // 金额默认以分为单位，若为整数后面自动补零
+    // str 分单位金额
+    fotmatMoney(str) {
+      let _money = (str/100).toFixed(2);
+      /* if (!/\./.test(_money)) { // 为整数字符串在末尾添加.00
+        _money += '.00'
+      } */
+      return _money
     }
 
 }
